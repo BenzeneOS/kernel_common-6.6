9f04cbe0a9f2acfad874242b33fe9cf339d01595
Amaan Qureshi <git@amaanq.com>
2026-08-05 16:08:22 -0400

    proc: hide root tracers from `TracerPid`
    An app treats a non-zero `TracerPid` as proof a debugger is attached,
    and only root can trace an app here, so keying on the tracer uid hides
    benzened while leaving the run-as path ordinary app debugging uses reporting
    honestly.

--- fs/proc/array.c
+++ fs/proc/array.c
@@ -161,7 +161,7 @@ static inline void task_state(struct seq_file *m, struct pid_namespace *ns,
 		task_tgid_nr_ns(rcu_dereference(p->real_parent), ns) : 0;
 
 	tracer = ptrace_parent(p);
-	if (tracer)
+	if (tracer && !uid_eq(task_uid(tracer), GLOBAL_ROOT_UID))
 		tpid = task_pid_nr_ns(tracer, ns);
 
 	tgid = task_tgid_nr_ns(p, ns);
